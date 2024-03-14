cbb976dbb6e008885938b412192a26a46a9b8582
Artemii <angap4@gmail.com>
2024-03-14 22:48:28 +1000

    v1

--- Lagrange/test.py
+++ Lagrange/test.py
@@ -1,32 +1,51 @@
-import pandas as pd
-import sympy
+from numpy import pi, sin
 
 
-def lagrange(points: pd.DataFrame, bp: float) -> float:
-    count_points = len(points['x'])
+def lagrange(points: list[tuple[float, float]], bp: float) -> float:
+    count_points = len(points)
     result = 0
-    for k in range(count_points):
-        point = points.iloc[[k]]
-        x = point['x'][k]
-        y = point['y'][k]
-        point = [x, y]
+    for k, point in enumerate(points):
         multiply = 1
         for j in range(k - 1):
-            sub_point = points.iloc[[j]]
-            x = sub_point['x'][j]
+            x = points[j][0]
             multiply *= ((bp - x) / (point[0] - x))
         for i in range(k + 1, count_points):
-            sub_point = points.iloc[[i]]
-            x = sub_point['x'][i]
+            x = points[i][0]
             multiply *= ((bp - x) / (point[0] - x))
         result += multiply * point[1]
     return result
 
 
-def f1(x: float) -> float:
-    return abs(x)
+def generate_points(rng: tuple[float, float], count_points: int, function) -> list[tuple[float, float]]:
+    step = (rng[1] - rng[0]) / (count_points - 1)
+    res = [(rng[0], function(rng[0], count_points))]
+    for i in range(1, count_points - 1):
+        x = rng[0] + step * i
+        res.append((x, function(x, count_points)))
+    res.append((rng[1], function(rng[1], count_points)))
+    return res
 
 
-df = pd.read_csv("points.csv")
+# В = 15
+# y=2x− cos(x)
+def func(x: float, n: int = 5) -> float:
+    """
+    Функция
 
-print(lagrange(df, -1, f1), f1(-1))
+    :param x: Точка x
+    :param n: Степень производной. Считается от 5 для удобства.
+    :return: Значение функции в точке x
+
+    """
+    return sin(x + (n * pi) / 2)
+
+
+count_pts = 800
+
+range_graph = (-pi, pi)
+
+points = generate_points(range_graph, count_pts, func)
+
+bp = pi/2
+
+print(f"{bp:.10f}", f"{lagrange(points, bp):.10f}", f"{func(bp, count_pts):.10f}")
