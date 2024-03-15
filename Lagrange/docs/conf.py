e72d3fc337766bae2aac45a7fdf5fec72bb657f7
Artemii <angap4@gmail.com>
2024-03-15 11:58:57 +1000

    init doc

---
+++ Lagrange/docs/conf.py
@@ -0,0 +1,31 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath('../'))
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Полином Лагранжа'
+copyright = '2024, Артемий Поповкин'
+author = 'Артемий Поповкин'
+release = '0.7.5'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['sphinx.ext.autodoc']
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+language = 'ru'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
---
+++ Lagrange/docs/index.rst
@@ -0,0 +1,22 @@
+.. Полином Лагранжа documentation master file, created by
+   sphinx-quickstart on Fri Mar 15 11:25:19 2024.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Документация Полинома Лагранжа
+==============================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Документация
+
+    test
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
