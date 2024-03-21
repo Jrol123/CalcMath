923f1419febd901e6a27f735d5fca555a8ef95e0
Artemii <angap4@gmail.com>
2024-03-21 16:27:20 +1000

    1-st part done

--- Lagrange/test.py
+++ Lagrange/test.py
@@ -1,4 +1,6 @@
-from numpy import pi, cos
+from numpy import cos, linspace, pi
+from math import factorial
+import matplotlib.pyplot as plt
 
 
 def lagrange(points: list[tuple[float, float]], bp: float) -> float:
@@ -66,6 +68,7 @@ def func(x: float):
     """
     return 2 * x - cos(x)
 
+
 def derv_func(x: float, n: int = 2) -> float:
     """
     Вторая производная функции y = 2x− cos(x)
@@ -82,17 +85,47 @@ def derv_func(x: float, n: int = 2) -> float:
     return cos(x + ((n - 2) * pi) / 2)
 
 
-count_pts = 6
+def get_norm(function, rng: tuple[float, float], *args) -> float:
+    return max(abs(function(linspace(*rng), *args)))
+
+
+def abs_error(norm: float, lagr: float) -> float:
+    return abs(norm - lagr)
+
+
+def rel_error(abs_er: float, norm: float) -> float:
+    return (abs_er / norm) * 100
+
+
+def teor_error(count_pts: int, rng: tuple[float, float], der_norm: float):
+    return (get_norm(derv_func, rng, count_pts + 1) / factorial(count_pts + 1)) * ((rng[1] - rng[0])**(count_pts + 1))
+
+
+count_pts = 20
 """
 Количество точек, которое нужно сгенерировать.
 
 Должно быть >= 2, так как по условию берутся граничные точки тоже.
 """
 
-range_graph = (-10.0, 10.0)
+range_graph = (-30.0, 30.0)
 
 full_points = generate_points(range_graph, count_pts, func)
 
 keypoint = 0.0
 
-print(f"{keypoint:.10f}", f"{lagrange(full_points, keypoint):.10f}", f"{func(keypoint):.10f}")
+# print(f"{keypoint:.10f}", f"{lagrange(full_points, keypoint):.10f}", f"{func(keypoint):.10f}")
+
+norm = get_norm(func, range_graph)
+lag_norm = max(abs(lagrange(full_points, linspace(*range_graph))))
+abs_e = max(abs(lagrange(full_points, linspace(*range_graph)) - func(linspace(*range_graph))))
+der_e = max(abs(derv_func(linspace(*range_graph))))
+rel_e = rel_error(abs_e, lag_norm)
+ter_e = teor_error(count_pts, range_graph, der_e)
+
+print(norm, lag_norm, der_e, abs_e, rel_e, ter_e)
+
+# TODO: Сделать цикл для построения графика
+# TODO: Сделать вывод (q)
+
+plt.plot(linspace(range_graph))
