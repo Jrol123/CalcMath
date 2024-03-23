b659a73b0d915992b245275f7831b9d61a155f7c
Artemii <angap4@gmail.com>
2024-03-23 22:38:32 +1000

    done

--- Lagrange/test.py
+++ Lagrange/test.py
@@ -1,9 +1,9 @@
 from numpy import cos, linspace, pi
 from math import factorial
-import matplotlib.pyplot as plt
+import pandas as pd
 
 
-def lagrange(points: list[tuple[float, float]], bp: float) -> float:
+def lagrange(bp: float, points: list[tuple[float, float]]) -> float:
     """
     Неньютоновская реализация полинома Лагранжа
 
@@ -45,12 +45,9 @@ def generate_points(rng: tuple[float, float], count_points: int, function) -> li
     :rtype: list[tuple[float, float]]
 
     """
-    step = (rng[1] - rng[0]) / (count_points - 1)
-    res = [(rng[0], function(rng[0]))]
-    for i in range(1, count_points - 1):
-        x = rng[0] + step * i
-        res.append((x, function(x)))
-    res.append((rng[1], function(rng[1])))
+    res = []
+    for pos in linspace(*rng, count_points):
+        res.append((pos, function(pos)))
     return res
 
 
@@ -71,12 +68,13 @@ def func(x: float):
 
 def derv_func(x: float, n: int = 2) -> float:
     """
-    Вторая производная функции y = 2x− cos(x)
+    Вторая-n производная функции y = 2x− cos(x)
 
     От второй производной далее, по тригонометрическим свойствам, можно считать n-ю производную
 
     :param x: Точка x
     :param n: Степень производной. Считается от 2 для удобства.
+        + Потому что именно на 2-й производной убирается константа
     :return: Значение функции в точке x
 
     :rtype: float
@@ -86,46 +84,75 @@ def derv_func(x: float, n: int = 2) -> float:
 
 
 def get_norm(function, rng: tuple[float, float], *args) -> float:
-    return max(abs(function(linspace(*rng), *args)))
+    """
+    Получение нормы функции
+
+    :param function: Некоторая функция, норму которой мы хотим получить
+    :param rng: Кортеж границ
+    :param args: Дополнительные аргументы функции
+
+    :return: Норма функции
+    :rtype: float
+
+    """
+    return max(abs(function(linspace(*rng, num=10**3), *args)))
 
 
-def abs_error(norm: float, lagr: float) -> float:
-    return abs(norm - lagr)
+def rel_error(abs_er: float, norm_f: float) -> float:
+    """
+    Получение относительной ошибки
+
+    :param abs_er: Абсолютная ошибка
+    :param norm_f: Норма функции
+
+    :return: Относительная ошибка
+    :rtype: float
+
+    """
+    return (abs_er / norm_f) * 100
 
 
-def rel_error(abs_er: float, norm: float) -> float:
-    return (abs_er / norm) * 100
+def teor_error(count_points: int, rng: tuple[float, float]) -> float:
+    """
+    Получение теоретической ошибки
+
+    :param count_points: Количество точек
+    :param rng: Кортеж границ
+
+    :return: Теоретическая ошибка
+    :rtype: float
+
+    """
+    return (get_norm(derv_func, rng, count_points + 1) / factorial(count_points + 1)) * (
+            (rng[1] - rng[0]) ** (count_points + 1))
 
 
-def teor_error(count_pts: int, rng: tuple[float, float], der_norm: float):
-    return (get_norm(derv_func, rng, count_pts + 1) / factorial(count_pts + 1)) * ((rng[1] - rng[0])**(count_pts + 1))
+rng_pos = [3, 5, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+abs_e_mass = []
+rel_e_mass = []
+ter_e_mass = []
+print("n", "", "Абсолютная норма", "Относительная норма", "Теоретическая норма", sep='\t')
+for count_pts in rng_pos:
+    range_graph = (0.1, 0.6)
 
+    full_points = generate_points(range_graph, count_pts, func)
 
-count_pts = 20
-"""
-Количество точек, которое нужно сгенерировать.
+    norm = get_norm(func, range_graph)
+    lag_norm = get_norm(lagrange, range_graph, full_points)
+    # lag_norm = max(abs(lagrange(linspace(*range_graph), full_points)))
+    # print(sub_lag_norm, lag_norm)
+    abs_e = max(abs(lagrange(linspace(*range_graph, num=10**3), full_points) - func(linspace(*range_graph, num=10**3))))
+    # der_e = max(abs(derv_func(linspace(*range_graph))))
+    der_e = get_norm(derv_func, range_graph)
+    rel_e = rel_error(abs_e, lag_norm)
+    ter_e = teor_error(count_pts, range_graph)
 
-Должно быть >= 2, так как по условию берутся граничные точки тоже.
-"""
+    abs_e_mass.append(abs_e)
+    rel_e_mass.append(rel_e)
+    ter_e_mass.append(ter_e)
 
-range_graph = (-30.0, 30.0)
+    print(count_pts, "", abs_e, rel_e, ter_e, sep='\t')
 
-full_points = generate_points(range_graph, count_pts, func)
-
-keypoint = 0.0
-
-# print(f"{keypoint:.10f}", f"{lagrange(full_points, keypoint):.10f}", f"{func(keypoint):.10f}")
-
-norm = get_norm(func, range_graph)
-lag_norm = max(abs(lagrange(full_points, linspace(*range_graph))))
-abs_e = max(abs(lagrange(full_points, linspace(*range_graph)) - func(linspace(*range_graph))))
-der_e = max(abs(derv_func(linspace(*range_graph))))
-rel_e = rel_error(abs_e, lag_norm)
-ter_e = teor_error(count_pts, range_graph, der_e)
-
-print(norm, lag_norm, der_e, abs_e, rel_e, ter_e)
-
-# TODO: Сделать цикл для построения графика
-# TODO: Сделать вывод (q)
-
-plt.plot(linspace(range_graph))
+abs_df = pd.DataFrame(abs_e_mass, rng_pos, columns=["Value"])
+rel_df = pd.DataFrame(rel_e_mass, rng_pos, columns=["Value"])
+ter_df = pd.DataFrame(ter_e_mass, rng_pos, columns=["Value"])
