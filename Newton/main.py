bb7dc7a2af4947ec53dc0f2b8e5490a35e6db829
Artemii <angap4@gmail.com>
2024-03-29 16:48:15 +1000

    first try

--- Newton/main.py
+++ Newton/main.py
@@ -1,9 +1,8 @@
 from numpy import cos, sin, pi, linspace
 
-COUNT_POINTS = 10
-RANGE = (0.1, 0.6)
 
-
+# 0.44, 0.13, 0.56, 0.37
+# (0.1, 0.6)
 # В = 15
 def func(x: float, deriv_s: int = 0):
     """
@@ -22,8 +21,33 @@ def func(x: float, deriv_s: int = 0):
         return 2 * x - cos(x)
     elif deriv_s == 1:
         return x + sin(x)
-    else:
-        return cos(x + ((deriv_s - 2) * pi) / 2)
+    return cos(x + ((deriv_s - 2) * pi) / 2)
 
 
-def newton(x: float, deriv_s: int = 0):
+def newton(rng: tuple[float, float], step: float, count_pts: int, is_fwd: bool = True):
+    pass
+
+
+def check_pos(point: float, points: list[float]) -> tuple:
+    if point in points:
+        # Если точка находится в массиве точек
+        return 200, point
+
+    if point < points[0]:
+        return -500, None
+    elif point > points[-1]:
+        return 500, None
+
+    if point < points[1]:
+        return -1
+    elif point > points[-2]:
+        return 1
+
+
+
+count_points = 10
+range_graph = (0.1, 0.6)
+full_points = linspace(*range_graph, count_points)
+print(check_pos(range_graph, 0.1))
+if isinstance(1, float):
+    print("flt")
