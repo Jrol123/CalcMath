76857ab0f8d36e4fe60f11064b67786cba250399
Artemii <angap4@gmail.com>
2024-06-01 19:53:07 +1000

    done

--- CalcMethod/main.py
+++ CalcMethod/main.py
@@ -9,74 +9,91 @@ from datetime import datetime, timedelta
 
 
 class OutputEntity:
-    def __init__(self, name_function: str, count_iterations: int, precision: float,  time: timedelta):
+    def __init__(self, name_function: str, true_result: float, result: float, count_iterations: int, precision: float,
+                 time: timedelta, mass_results: list[float]):
         self.name_function = name_function
+        self.true_result = true_result
+        self.result = result
         self.count_iterations = count_iterations
         self.precision = precision
         self.time = time
+        self.results = mass_results
 
     def __str__(self):
-        return (f"\nФункция:\t{self.name_function}\nКоличество итераций:\t{self.count_iterations}\n"
-                f"Использованная точность:\t{self.precision}\nЗатраченное время:\t{self.time}")
+        return (
+            f"\nФункция:\t{self.name_function}\n"
+            f"Desmos результат:\t{self.true_result}\nПолученный результат:\t{self.result}\n"
+            f"Разница:\t{abs(self.true_result - self.result)}\n"
+            f"Количество итераций:\t{self.count_iterations}\nИспользованная точность:\t{self.precision}\n"
+            f"Затраченное время:\t{self.time}\n")
 
 
-def secant(f, x_prev: float, x_main: float) -> float:
+def dichotomy(f, x_mass: list[float, float, float]) -> list[float, float, float]:
+    mid = (x_mass[1] + x_mass[0]) / 2
+    x_mass[2] = mid
+    if f(mid) * f(x_mass[0]) < 0:
+        x_mass[1] = mid
+    else:
+        x_mass[0] = mid
+    return x_mass
+
+
+def secant(f, x_mass: tuple[float, float, float]) -> float:
     """
     Метод хорд
 
     :param f: Функция
     :type f: function
-    :param x_prev: Предыдущее значение x
-    :param x_main: Значение x, откуда идёт счёт
+    :param x_mass: Массив значений x, состоящий из левого края [0], правого края [1], и последнего значения [-1]
 
     :return: Значение x
 
     """
+    x_prev = x_mass[-1]
+    x_main = x_mass[0]
     return x_main - (f(x_main) * (x_prev - x_main)) / (f(x_prev) - f(x_main))
 
 
-def newton(f, x_prev: float, x_main) -> float:
+def newton(f, x_mass: tuple[float, float, float]) -> float:
     """
 
     :param f: Функция
     :type f: function
-    :param x_prev: Предыдущее значение x
+    :param x_mass: Массив значений x. Состоит из левого края [0], правого края [1], и последнего значения [-1]
 
     :return: Значение x
 
     """
+    x_prev = x_mass[-1]
     return x_prev - f(x_prev) / f(x_prev, 1)
 
 
-def get_result(function, mass_borders, eps):
+def get_result(function, mass_borders, eps) -> OutputEntity:
     """
     Получение результатов из функций
 
     :param function:
     :type function: function
     :param mass_borders:
-    :param eps: Точность
+    :param eps: Точность измерений
 
-    :return:
+    :return: Различные параметры, полученные при вычислении корня.
 
     """
-    x_main = mass_borders[0]
-    x_prev = 0
-    if function.__name__ == secant.__name__:
-        x_prev = mass_borders[1]
-    else:
-        x_prev = x_main
+    mass_val = [mass_borders[0], mass_borders[1], mass_borders[-1]]
+    end_mass = [mass_val[-1]]
     delta = abs(mass_borders[0] - mass_borders[1])
     i = 0
     date_start = datetime.now()
     while delta > eps:
-        x_cur = function(func, x_prev, x_main)
-        print(i := i + 1, x_prev, x_cur)
-        delta = abs(x_cur - x_prev)
-        x_prev = x_cur
+        x_cur = function(func, mass_val)
+        print(i := i + 1, mass_val[-1], x_cur)
+        delta = abs(x_cur - mass_val[-1])
+        mass_val[-1] = x_cur
+        end_mass.append(x_cur)
     date_end = datetime.now()
     # print((date_end - date_start))
-    return OutputEntity(function.__name__, i, eps, (date_end - date_start))
+    return OutputEntity(function.__name__, -0.5373, mass_val[-1], i, eps, (date_end - date_start), end_mass)
 
 
 # В - 15
@@ -101,5 +118,24 @@ def func(x: float, deriv_s: int = 0):
 if __name__ == '__main__':
     # Start here
     range_mass = [-0.75, -0.25]  # Массив границ
-    eps = 1e-100
-    print(get_result(secant, range_mass, eps))
+    eps = 1e-10
+    sec_res = get_result(secant, range_mass, eps)
+    new_res = get_result(newton, range_mass, eps)
+
+    delta = abs(range_mass[0] - range_mass[1])
+    mass_val = [range_mass[0], range_mass[1], range_mass[-1]]
+    mass_res = [(range_mass[1] + range_mass[0]) / 2]
+    prev_stat = 1
+    i = 0
+    date_start = datetime.now()
+    while delta > eps:
+        mass_val = dichotomy(func, mass_val)
+        delta = abs(mass_val[-1] - prev_stat)
+        print(i := i + 1, mass_val, delta)
+        prev_stat = mass_val[-1]
+        if func(prev_stat) == 0:
+            break
+    date_end = datetime.now()
+    dich_res = OutputEntity(dichotomy.__name__, -0.5373, prev_stat, i, eps, (date_end - date_start), mass_val)
+
+    print(sec_res, new_res, dich_res)
